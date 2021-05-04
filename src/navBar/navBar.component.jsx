fa1a2967586bf976604d286818a1e72c956603ea
gonsaje <song.jaehyun@gmail.com>
2021-05-03 20:40:09 -0400

    style/format fix 1

--- src/navBar/navBar.component.jsx
+++ src/navBar/navBar.component.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import './navBar.styles.css';
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import {faUserCircle} from "@fortawesome/free-regular-svg-icons";
+import './navBar.styles.scss';
+import {faDna, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {faUserCircle, faUser} from "@fortawesome/free-regular-svg-icons";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const NavBar = (props) => {
 
     return (
         <div className="nav-bar-container">
-            <h1>GENOME.</h1>
-            <div className="search-bar">
-                <input type='search' placeholder="EXPLORE."></input>
-                <button>
-                <FontAwesomeIcon icon={faSearch} />
-                <i class="fas fa-search"></i>
-                </button>
-            </div>
-            <div>
-                <FontAwesomeIcon icon={faUserCircle} />
-                <button>sign up</button>
+            <div className="nav-bar">
+                <h1>GENOME.</h1>
+                <div className="menu-icons">
+                    {/* <div class="container">
+                        <input type="text" placeholder="EXPLORE."/>
+                        <div class="search"></div>
+                    </div> */}
+                    {/* {/* <FontAwesomeIcon icon={faUser} /> */}
+                    <ul >
+                        <li><FontAwesomeIcon className="dropdown"icon={faDna} /> 
+                            <ul className='dropdown-menu'>
+                                <li>Profile</li>
+                                <li>Settings</li>
+                                <li>Sign Up/Log In</li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </div>           
             </div>
         </div>
     )
--- src/profilePage/profile.component.jsx
+++ src/profilePage/profile.component.jsx
@@ -10,14 +10,21 @@ const Profile = () => {
             </div>
             <div className="bio-containter">
                 <div className ="headline">
-                    <h1>Hablot Brown</h1>
-                    <h3>1200 wax.</h3>
-                </div>
+                    <div className="name">
+                        <h1>Hablot Brown</h1>
+                        <h4>1200 wax</h4>
+                    </div>
                 <div className="bio">
                     For three young guys who left college to pursue a music career, Hablot Brown produce eminently sophisticated, mature music. Whether you call their style neo-soul or R&B, the group’s tunes are full of profoundly expressive vocals, lockdown rhythms, and polished production. It’s that potent mix that saw the band’s first two singles, “Gone” and “Just Call,” streamed more than 1.5 million combined times in just twelve months, big numbers that helped earn them an opening spot on Tom Misch’s recent tour. Fresh of that experience, and still releasing hot songs like “Little Bit,” Hablot Brown recently took the time to talk about their inspirations, creative process, touring, and style with O.N.S.
                 </div>
+                </div>
                 <div className="social-links">
-
+                    <a class=" fa fa-facebook"></a>
+                    <a class=" fa fa-instagram"></a>
+                    <a class=" fa fa-twitter"></a>
+                    <a class=" fa fa-spotify"></a>
+                    <a class=" fa fa-soundcloud"></a>
+                    <a class=" fa fa-applemusic"></a>
                 </div>
             </div>
             <div className="statement-container">
@@ -27,7 +34,7 @@ const Profile = () => {
                     Unfortunately there are no concert dates for Hablot Brown scheduled in 2021.
                     Songkick is the first to know of new tour announcements and concert information, so if your favorite artists are not currently on tour, join Songkick to track Hablot Brown and get concert alerts when they play near you, like 11954 other Hablot Brown fans.
                 </div>
-
+                <button>Become A Supporter</button>
 
             </div>
         </div>
